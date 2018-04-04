048e89e76de12d64c449edf56b078bcc8e2a8075
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2018-04-04 16:48:37 +0200

    Init

--- src/SimpleES.Api/Startup.cs
+++ src/SimpleES.Api/Startup.cs
@@ -29,7 +29,7 @@ namespace SimpleES.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.EnvironmentName == "local")
             {
                 app.UseDeveloperExceptionPage();
             }
